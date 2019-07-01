1ff05b88d3afcdb408b9da7a291c2430088cf182
et-nik <nikita.hldm@gmail.com>
2019-07-02 00:00:07 +0300

    Add GameAP Daemon installation test

--- run_test.sh
+++ run_test.sh
@@ -17,3 +17,9 @@ echo
 echo "Checking available gameap host"
 echo
 curl -sL -w "HTTP CODE: %{http_code}\\n" "http://test.gameap/login" -o /dev/null
+
+echo
+echo "Checking GameAP Daemon installation"
+
+echo "Illuminate\Support\Facades\Cache::put('gdaemonAutoSetupToken', 'fake', 300);" | /var/www/gameap/artisan tinker
+export createToken=fake; export panelHost=http://test.gameap; ./debian/install-gdaemon-en.sh | bash --
