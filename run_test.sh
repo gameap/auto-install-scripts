6ec8186df46502228955d8603309e51c708f7673
et-nik <nikita.hldm@gmail.com>
2021-03-13 22:37:53 +0300

    Rename vars

--- run_test.sh
+++ run_test.sh
@@ -115,9 +115,6 @@ fi
 
 echo "Illuminate\Support\Facades\Cache::put('gdaemonAutoCreateToken', 'test_auto_setup_token', 99999);" | /var/www/gameap/artisan tinker || true
 
-export createToken=test_auto_setup_token # Deprecated name
-export panelHost=http://test.gameap;     # Deprecated name
-
 export CREATE_TOKEN=test_auto_setup_token
 export PANEL_HOST=http://test.gameap;
 
