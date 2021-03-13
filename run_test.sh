38b0cb687d2c4abbbd6f7ca17bf5d8ebb2e281f4
et-nik <nikita.hldm@gmail.com>
2021-03-13 20:54:41 +0300

    Set auto setup token cache value

--- run_test.sh
+++ run_test.sh
@@ -113,6 +113,12 @@ else
     exit 1
 fi
 
+if ! echo "Illuminate\Support\Facades\Cache::put('gdaemonAutoCreateToken', 'test_auto_setup_token', 99999);" | /var/www/gameap/artisan tinker; then
+    echo "Failed to set auto setup token"
+    ./artisan --version
+    exit 1
+fi
+
 export createToken=test_auto_setup_token
 export panelHost=http://test.gameap;
 
