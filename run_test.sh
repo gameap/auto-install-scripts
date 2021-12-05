d6df9b110abe0ddc6ddccee6943b66174f43ac35
et-nik <nikita.hldm@gmail.com>
2021-12-05 21:33:35 +0300

    fix debian dist detecting

--- run_test.sh
+++ run_test.sh
@@ -39,16 +39,15 @@ detect_os ()
     elif [[ -n "$(command -v lsb_release > /dev/null 2>&1)" ]]; then
         dist=$(lsb_release -c | cut -f2)
         os=$(lsb_release -i | cut -f2 | awk '{ print tolower($1) }')
+    fi
 
-    elif [[ -e /etc/debian_version ]]; then
+    if [[ -z "$dist" ]] && [[ -e /etc/debian_version ]]; then
         os=$(cat /etc/issue | head -1 | awk '{ print tolower($1) }')
         if grep -q '/' /etc/debian_version; then
             dist=$(cut --delimiter='/' -f1 /etc/debian_version)
         else
             dist=$(cut --delimiter='.' -f1 /etc/debian_version)
         fi
-    else
-        unknown_os
     fi
 
     if [[ -z "$dist" ]]; then
@@ -76,6 +75,12 @@ detect_os ()
     dist=${dist,,}
 
     echo "Detected operating system as $os/$dist."
+}
+
+unknown_os ()
+{
+    echo "Unfortunately, your operating system distribution and version are not supported by this script."
+    exit 1
 }
 
 echo
@@ -132,9 +137,3 @@ if ! ${daemon_install_command}; then
 
     exit 1
 fi
-
-unknown_os ()
-{
-    echo "Unfortunately, your operating system distribution and version are not supported by this script."
-    exit 1
-}
