b479afc2baec5c0312331319f14b5d66ff6ea927
Nikita <nikita.hldm@gmail.com>
2023-01-21 13:14:13 +0100

    selinux changes

--- centos/install-en.sh
+++ centos/install-en.sh
@@ -850,6 +850,8 @@ main ()
     chcon -R -t httpd_sys_content_t ${gameap_path}
     chcon -R -t httpd_sys_rw_content_t ${gameap_path}/storage
     chcon -R -t httpd_sys_rw_content_t ${gameap_path}/modules
+    setsebool -P httpd_can_network_connect 1
+    setsebool -P httpd_can_network_connect_db 1
 
     case ${db_selected} in
         "mysql" )
